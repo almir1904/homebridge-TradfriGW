466aeab99f6410a37b6da734f343e0f1b1590815
almir1904 <almir1904@users.noreply.github.com>
2021-02-21 20:57:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ module.exports = function(homebridge) {
     // Load .env
     require('dotenv').config({path: Path.join(process.env.HOME, '.homebridge/.env')});
 
-    homebridge.registerPlatform('homebridge-ikea-gateway', 'TradfriGW', require('./src/platform.js'));
+    homebridge.registerPlatform('homebridge-TradfriGW', 'TradfriGW', require('./src/platform.js'));
 };
